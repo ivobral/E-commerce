756e1d5c2086f092ee225fc7d0cfd056fb503c52
ivobral <ivobral@hotmail.com>
2021-07-19 16:02:13 +0200

    Cart badge number increasing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import logo from '../../assets/e-commerce.png';
 import useStyles from './styles';
 
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
     const classes = useStyles();
     
     return (
@@ -20,7 +20,7 @@ const Navbar = () => {
                         <div className={classes.grow} />
                         <div className={classes.button}>
                             <IconButton aria-label="Show cart items" color="inherit">
-                                <Badge badgeContent={4} color="secondary">
+                                <Badge badgeContent={totalItems} color="secondary">
                                     <ShoppingCart />
                                 </Badge>
                             </IconButton>
